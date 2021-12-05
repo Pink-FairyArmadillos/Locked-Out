d2fea616073929eca5fbfbac7a5457484f5817d2
Abeer Faizan <abeerfaizan@gmail.com>
2021-12-04 21:27:28 -0500

    Added model and did cleanup

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,13 @@
 const passwordController = require('../controllers/passwordController');
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
+
+router.get('/', (req, res) => {
+    return res.status(200).json("");
+});
 
 router.get('/login', passwordController.getLogin, (req, res) => {
     return res.status(200).json(res.locals.userExists)
--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const passwordController = require('./controllers/passwordController');
 const apiRouter = require('./routes/api');
+
 app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../build' ))); 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../build')));
 
 
-// app.get('/', (req, res) => {
-//   return res.status(200).json("");
-// });
- app.use('/api', apiRouter);
+app.get('/', (req, res) => {
+  return res.status(200).json("");
+});
+app.use('/api', apiRouter);
 
-app.listen(3000); 
+app.listen(3000);
