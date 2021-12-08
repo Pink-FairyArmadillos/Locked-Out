d2fc38958a84b117d0c79ec06d66f9b51c13b991
samflam <me@samflam.com>
2021-12-07 17:07:22 -0800

    Added testing boilerplate

---
+++ __tests__/db.js
@@ -0,0 +1,5 @@
+
+
+//implement test database model
+//use password.db to initialize a db and schema as part of the test suite
+
---
+++ __tests__/supertest.js
@@ -0,0 +1,93 @@
+const request = require("supertest");
+
+const server = "http://localhost:3000";
+
+/**
+ * Read the docs! https://www.npmjs.com/package/supertest
+ */
+
+describe("Route integration", () => {
+  describe("/", () => {
+    describe("GET", () => {
+      // Note that we return the evaluation of `request` here! It evaluates to
+      // a promise, so Jest knows not to say this test passes until that
+      // promise resolves. See https://jestjs.io/docs/en/asynchronous
+      it("responds with 200 status and text/html content type", () => {
+        return request(server)
+          .get("/")
+          .expect("Content-Type", /text\/html/)
+          .expect(200);
+      });
+    });
+  });
+
+  describe("/api/login", () => {
+    describe("GET", () => {
+      it("responds with 200 status and application/json content type", () => {
+        return request(server)
+          .get("/api/login")
+          .expect("Content-Type", /json/)
+          .expect(200);
+      });
+
+      it('parses an object from the response to login', () => {
+        return request(server)
+          .get("/api/login")
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(typeof res.locals.userMetadata).toEqual("object");
+          });
+      });
+    });
+  });
+
+  //post test goes here
+  describe("/api/signup", () => {
+    describe("POST", () => {
+      it("responds with 200 status and application/json content type", () => {
+        return request(server)
+          .get("/api/signup")
+          .expect("Content-Type", /json/)
+          .expect(200);
+      });
+
+      it('parses an object from the response to signup', () => {
+        return request(server)
+          .get("/api/signup")
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(typeof res.locals.userMetadata).toEqual("object");
+          });
+      });
+    });
+  });
+
+  //put test goes here
+  describe("/api/addEntry", () => {
+    describe("POST", () => {
+      it("responds with 200 status and application/json content type", () => {
+        return request(server)
+          .get("/api/addEntry")
+        //   .expect("Content-Type", /json/)
+          .expect(200);
+      });
+
+      it('parses an array from the response to login', () => {
+        return request(server)
+          .get("/api/addEntry")
+        //   .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(isArray(res.locals.entries)).toEqual(true);
+          });
+      });
+    });
+  });
+
+  //delete test goes here
+
+
+  //test signup goes here
+});
