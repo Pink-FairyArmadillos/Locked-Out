5040fb6afd9bd0eb551f53d7120de58700e2ec57
Zachary Lim <lzzhengyi@gmail.com>
2021-12-08 17:50:42 -0500

    signup, login, set session id refactored and functional

--- usersTable.sql
+++ usersTable.sql
@@ -2,10 +2,11 @@ DROP TABLE users;
 CREATE TABLE users (
     _id SERIAL NOT NULL PRIMARY KEY,  
     username VARCHAR,
-    password VARCHAR
+    passcode VARCHAR,
+    session_id uuid
 );
 
-INSERT INTO users (username, password)
+INSERT INTO users (username, passcode)
 VALUES (
     'test',
     'Password123'
