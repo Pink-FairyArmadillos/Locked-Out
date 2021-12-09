ea50fc1571eb6a27d2849434368ac80a0e817ff7
Sam Pyo <samhpyo@gmail.com>
2021-12-08 20:32:59 -0800

    facebookOAUTH

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,97 +1,55 @@
-import React, { useState, useEffect } from "react";
-import Dashboard from "./Dashboard.jsx";
-import store from "../store";
-import PasswordStrengthMeter from "../components/PasswordStrengthMeter.jsx";
+import React, { useState } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import { Card, Image } from 'react-bootstrap';
+// import './App.css';
+let username;
 
-const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
-  const [passwordState, setPasswordState] = useState('password');
+function Login() {
 
-  const handleUserFetch = (data) => {
-    setUserLoggedIn(data.userExists);
-    store.dispatch({
-      type: "ADD_USER_ID",
-      payload: data.userID,
-    });
-  };
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState('');
 
-  const handleLogin = (username, password) => {
-    fetch(`/api/login?username=${username}&passwordUser=${password}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => handleUserFetch(data));
-  };
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+    username = response.accessToken;
+    console.log(username)
+  }
 
   return (
-    <div>
-      {!userLoggedIn && (
-        <div className="loginPage">
-          <img
-            style={{ marginTop: "3px", height: "4em", width: "4em" }}
-            src="pinkFairyArmidallo.png"
-            alt="Badass Armored PFA"
-          />
-          <h2 style={{ marginTop: "3px", marginLeft: "10px" }}>Login</h2>
-          <input
-            style={{ marginTop: "3px", marginLeft: "10px"}}
-            className="form-group"
-            placeholder="Username"
-            name="username"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          ></input>
-
-          <br></br>
-
-          <input
-            style={{ marginTop: "3px", marginLeft: "10px" }}
-            className="form-group shadow-none"
-            placeholder="Password"
-            type={passwordState}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-
-          <button style={{borderRadius: '8px', height: '2.5em', width: '8em', fontSize: '10px', padding:'0px', marginLeft:'1em'}} onClick={() => setPasswordState(passwordState === "password"?"text": "password")}>Reveal</button>
-
-          <PasswordStrengthMeter password={password} />
-
-          <button
-            style={{
-              marginTop: "3px",
-              marginLeft: "30px",
-              backgroundColor: "blue",
-              color: "white",
-              borderRadius: "4px",
-            }}
-            onClick={() => handleLogin(username, password)}
-          >
-            {" "}
-            Log in
-          </button>
-          <button
-            style={{
-              marginTop: "3px",
-              marginLeft: "10px",
-              borderRadius: "4px",
-            }}
-            disabled 
-            // onClick={() => handleSignup(username, password)}
-          >
-            Sign up
-          </button>
-        </div>
-      )}
-      {userLoggedIn && <Dashboard />}
+    <div class="container">
+      <Card style={{ width: '600px' }}>
+        <Card.Header>
+          {!login &&
+            <FacebookLogin
+              appId="772535610372466"
+              autoLoad={false}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={ responseFacebook }
+              icon="fa-facebook" />
+          }
+          {login &&
+            <Image src={picture} roundedCircle />
+          }
+        </Card.Header>
+        {login &&
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>
+              {data.email}
+            </Card.Text>
+          </Card.Body>
+        }
+      </Card>
     </div>
   );
-};
-
+}
 export default Login;
